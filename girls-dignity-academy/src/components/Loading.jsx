c7dfa17e123fa789e996fd66c06983033a0ecba7
willy clif <willyclif09@gmail.com>
2026-04-07 13:55:27 +0200

    Delete girls-dignity-academy directory

--- girls-dignity-academy/src/components/Loading.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-	return (
-		<div className="min-h-[40vh] flex items-center justify-center">
-			<div className="spinner" aria-label="Loading"></div>
-		</div>
-	)
-}
--- girls-dignity-academy/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-	// React JSX works via esbuild; plugin is optional for Fast Refresh which we can add later
-})
